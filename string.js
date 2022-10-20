ca2281497aae7bd9275ef256694f4b7ab918c967
Rodrigo Batista Alves Santos <A0153651@redecorp.br>
2022-10-20 14:02:02 -0300

    exemplos da aula1

---
+++ string.js
@@ -0,0 +1,12 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+
+const meuNome = "Meu nome é ";
+const meuNome2 = "Leonardo";
+const citacao = 'O Leo disse "oi!"';
+// Aspas diferentes para indicar qual vai aparecer no terminal e qual é da string
+console.log(meuNome + meuNome2);
+
+// concatenação (juntar strings)
+
+//template string OU template literal
